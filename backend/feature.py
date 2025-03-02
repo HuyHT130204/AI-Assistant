f529bc41da4b6387ab6231169a05dcd08176618d
HuyHT130204 <huyht1302@gmail.com>
2025-03-03 00:46:22 +0700

    First build

---
+++ backend/feature.py
@@ -0,0 +1,5 @@
+import playsound as playsound
+
+def playAssistantSound():
+    music_dir = "frontend\\assets\\audio\\start_sound.mp3"
+    playsound(music_dir)
---
+++ frontend/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    $('.text').textillate({
+        loop: true,
+        speed: 1500,
+        sync: true,
+        in: {
+          effect: "bounceIn",
+        },
+        out: {
+          effect: "bounceOut",
+        },
+    });
+
+    $(".siri-message").textillate({
+      loop: true,
+      sync: true,
+      in: {
+        effect: "fadeInUp",
+        sync: true,
+      },
+      out: {
+        effect: "fadeOutUp",
+        sync: true,
+      },
+    });
+
+    var siriWave = new SiriWave({
+      container: document.getElementById("siri-container"),
+      width: 940,
+      style: "ios9",
+      amplitude: "1",
+      speed: "0.30",
+      height: 200,
+      autostart: true,
+      waveColor: "#ff0000",
+      waveOffset: 0,
+      rippleEffect: true,
+      rippleColor: "#ffffff",
+    });
+});

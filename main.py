cb6420e3c5b1a6df23ab300e3eac4ceb75755746
HuyHT130204 <huyht1302@gmail.com>
2025-03-03 00:46:22 +0700

    First build

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+import eel
+from backend.feature import *
+
+eel.init("frontend")
+os.system('start msedge.exe --app="http://127.0.0.1:8000/index.html"')
+
+playAssistantSound()
+
+eel.start("index.html", mode=None, host="localhost", block=True)
